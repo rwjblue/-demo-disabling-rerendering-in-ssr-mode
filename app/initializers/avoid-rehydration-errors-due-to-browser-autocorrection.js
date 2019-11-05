ac3361c27c77f6d65ded2837133f978db17ad26b
Robert Jackson <rjackson@linkedin.com>
2019-11-04 23:36:33 -0500

    Document the patch.

--- app/initializers/avoid-rehydration-errors-due-to-browser-autocorrection.js
+++ app/initializers/avoid-rehydration-errors-due-to-browser-autocorrection.js
@@ -27,6 +27,22 @@ RehydrateBuilder.prototype.__closeBlock = function __closeBlock() {
   if (currentCursor.openBlockDepth === this.blockDepth) {
     currentCursor.openBlockDepth--;
 
+    // ********
+    // This conditional is the only difference between what is shipped in Ember
+    // 3.4+ and this patch. The rest of the code is duplicated in order to be
+    // able to add this guard.
+    //
+    // In the case of `<p><div>foo</div><p>` being sent by the FastBoot during
+    // serialization, the browser "corrects" that invalid HTML into
+    // `<p></p><div>foo</div><p></p>`. Unfortunately, this correction does
+    // **not** move our serialization markers so we end up with
+    // `currentCursor.nextSibling` being `null` when we do not expect it.
+    //
+    // This guard allows us to avoid throwing an error (`this.remove(null)`
+    // errors). The next opcode that runs will detect that the
+    // `currentCursor.candidate` is not what it expects and it will call `.clearMismatch`
+    // removing the incorrect (browser corrected) DOM and leaving the app in a
+    // functional state.
     if (currentCursor.nextSibling !== null) {
       currentCursor.candidate = this.remove(currentCursor.nextSibling);
     }
