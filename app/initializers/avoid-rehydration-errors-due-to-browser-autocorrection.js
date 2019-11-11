8d4e295f3dd446dcf4b79a5f719e56362dbcd666
Robert Jackson <rjackson@linkedin.com>
2019-11-10 21:57:46 -0500

    Update monkey patch based on @krisselden's review / tweaks.
    Fixes a bug in my original manua TS transpilation (forgot that `public
    foo` in a `constructor` means class field assigment).

--- app/initializers/avoid-rehydration-errors-due-to-browser-autocorrection.js
+++ app/initializers/avoid-rehydration-errors-due-to-browser-autocorrection.js
@@ -1,12 +1,139 @@
-import Ember from 'ember';
+/* globals location, MutationObserver */
 
-const { RehydrateBuilder, NewElementBuilder } = Ember.__loader.require('@glimmer/runtime');
+/**
+ * This is a monkey patch for Glimmer VM on Ember 3.4 will fix an issue where
+ * auto-corrected HTML failed to rehydrate and threw exceptions.
+ *
+ * In SSR, when a generated snippet of HTML is auto-corrected by browsers
+ * during parsing - for example `<p> <div> </div> </p>` gets parsed as
+ * `<p> </p> <div> </div>` by browsers - Glimmer was not able to properly
+ * consolidate the client-side DOM and the auto-corrected SSR DOM
+ * causing rehydration errors.
+ */
+import Ember from 'ember';
+import { registerDeprecationHandler, registerWarnHandler } from '@ember/debug';
+
+const IS_BROWSER = typeof document !== 'undefined';
+const REDEBUG = IS_BROWSER && location.search.indexOf('rehydrationDebug') > -1;
+
+if (IS_BROWSER && REDEBUG) {
+  /* eslint-disable-next-line no-console */
+  console.log(
+    'Disabling Ember.warn and Ember.deprecate to allow easier debugging'
+  );
+  registerWarnHandler(() => undefined);
+  registerDeprecationHandler(() => undefined);
+}
+
+if (IS_BROWSER && typeof FastBoot === 'undefined' && REDEBUG) {
+  const target = document.querySelector('.application-outlet');
+  const observer = new MutationObserver(function detectMutation(mutations) {
+    // check for removed target
+    mutations.forEach(function narrowDOMMutation(mutation) {
+      const nodes = Array.from(mutation.removedNodes);
+      const directMatch = nodes.indexOf(target) > -1;
+      const parentMatch = nodes.some(parent => parent.contains(target));
+      if (directMatch) {
+        /* eslint-disable-next-line no-console */
+        console.error('node', target, 'was directly removed!');
+      } else if (parentMatch) {
+        /* eslint-disable-next-line no-console */
+        console.error('node', target, 'was removed through a removed parent!');
+      }
+    });
+  });
+
+  const config = {
+    subtree: true,
+    childList: true,
+  };
+  /* eslint-disable-next-line ember/no-observers */
+  observer.observe(document.body, config);
+}
+
+const { RehydrateBuilder, NewElementBuilder } = Ember.__loader.require(
+  '@glimmer/runtime'
+);
+
+// from https://github.com/glimmerjs/glimmer-vm/blob/v0.35.11/packages/@glimmer/runtime/lib/vm/rehydrate-builder.ts#L440
+function isSeparator(node) {
+  return node.nodeType === 8 && node.nodeValue === '%|%';
+}
+
+// from https://github.com/glimmerjs/glimmer-vm/blob/v0.35.11/packages/@glimmer/runtime/lib/vm/rehydrate-builder.ts#L444
+function isEmpty(node) {
+  return node.nodeType === 8 && node.nodeValue === '% %';
+}
+
+// from https://github.com/glimmerjs/glimmer-vm/blob/v0.35.11/packages/@glimmer/runtime/lib/vm/rehydrate-builder.ts#L404
+function isTextNode(node) {
+  return node.nodeType === 3;
+}
+
+// from https://github.com/glimmerjs/glimmer-vm/blob/v0.35.11/packages/%40glimmer/runtime/lib/bounds.ts#L80
+function clear(bounds) {
+  const parent = bounds.parentElement();
+  const first = bounds.firstNode();
+  const last = bounds.lastNode();
+
+  let node = first;
+
+  while (node) {
+    const next = node.nextSibling;
+    parent.removeChild(node);
+    if (node === last) {
+      return next;
+    }
+    node = next;
+  }
+
+  return null;
+}
+
+// from https://github.com/glimmerjs/glimmer-vm/blob/v0.35.11/packages/@glimmer/runtime/lib/vm/element-builder.ts#L23
+class First {
+  constructor(node) {
+    this.node = node;
+  }
+
+  firstNode() {
+    return this.node;
+  }
+}
+
+// from https://github.com/glimmerjs/glimmer-vm/blob/v0.35.11/packages/@glimmer/runtime/lib/vm/element-builder.ts#L39
+class Last {
+  constructor(node) {
+    this.node = node;
+  }
+
+  lastNode() {
+    return this.node;
+  }
+}
+
+// https://github.com/glimmerjs/glimmer-vm/blob/v0.35.11/packages/@glimmer/runtime/lib/vm/rehydrate-builder.ts#L15
+class RehydratingCursor {
+  candidate = null;
+
+  injectedOmittedNode = false;
+
+  constructor(element, nextSibling, startingBlockDepth) {
+    this.element = element;
+    this.nextSibling = nextSibling;
+    this.startingBlockDepth = startingBlockDepth;
+    this.openBlockDepth = startingBlockDepth - 1;
+  }
+}
 
 // from https://github.com/glimmerjs/glimmer-vm/blob/v0.35.11/packages/@glimmer/runtime/lib/vm/element-builder.ts#L420-L497
 class RemoteBlockTracker {
   first = null;
+
   last = null;
+
   destroyables = null;
+
   nesting = 0;
 
   constructor(parent) {
@@ -14,7 +141,7 @@ class RemoteBlockTracker {
   }
 
   destroy() {
-    let { destroyables } = this;
+    const { destroyables } = this;
 
     if (destroyables && destroyables.length) {
       for (let i = 0; i < destroyables.length; i++) {
@@ -47,7 +174,9 @@ class RemoteBlockTracker {
   }
 
   didAppendNode(node) {
-    if (this.nesting !== 0) return;
+    if (this.nesting !== 0) {
+      return;
+    }
 
     if (!this.first) {
       this.first = new First(node);
@@ -57,7 +186,9 @@ class RemoteBlockTracker {
   }
 
   didAppendBounds(bounds) {
-    if (this.nesting !== 0) return;
+    if (this.nesting !== 0) {
+      return;
+    }
 
     if (!this.first) {
       this.first = bounds;
@@ -78,41 +209,19 @@ class RemoteBlockTracker {
   }
 }
 
-// from https://github.com/glimmerjs/glimmer-vm/blob/v0.35.11/packages/@glimmer/runtime/lib/vm/element-builder.ts#L23
-class First {
-  constructor(node) {
-    this.node = node;
-  }
-
-  firstNode() {
-    return this.node;
-  }
-}
-
-// from https://github.com/glimmerjs/glimmer-vm/blob/v0.35.11/packages/@glimmer/runtime/lib/vm/element-builder.ts#L39
-class Last {
-  constructor(node) {
-    this.node = node;
-  }
-
-  lastNode() {
-    return this.node;
-  }
-}
-
-// https://github.com/glimmerjs/glimmer-vm/blob/v0.35.11/packages/@glimmer/runtime/lib/vm/rehydrate-builder.ts#L15
-class RehydratingCursor {
-  candidate = null;
-  injectedOmittedNode = false;
-
-  constructor(element, nextSibling, startingBlockDepth) {
-    this.element = element;
-    this.nextSibling = nextSibling;
-    this.openBlockDepth = startingBlockDepth - 1;
-  }
-}
-
 /** start code from the changes in https://github.com/glimmerjs/glimmer-vm/pull/988/files */
+
+function isOpenBlock(node) {
+  return node.nodeType === 8 && node.nodeValue.lastIndexOf('%+b:', 0) === 0;
+}
+
+function isCloseBlock(node) {
+  return node.nodeType === 8 && node.nodeValue.lastIndexOf('%-b:', 0) === 0;
+}
+
+function getBlockDepth(node) {
+  return parseInt(node.nodeValue.slice(4), 10);
+}
 
 Object.defineProperty(RehydrateBuilder.prototype, 'candidate', {
   get() {
@@ -124,33 +233,55 @@ Object.defineProperty(RehydrateBuilder.prototype, 'candidate', {
   },
 
   set(node) {
-    let currentCursor = this.currentCursor;
+    const { currentCursor } = this;
 
     currentCursor.candidate = node;
-  }
+  },
 });
 
-RehydrateBuilder.prototype.disableRehydration = function(nextSibling) {
-  let currentCursor = this.currentCursor;
+RehydrateBuilder.prototype.disableRehydration = function patchedDisableRehydration(
+  nextSibling
+) {
+  const { currentCursor } = this;
 
   // rehydration will be disabled until we either:
   // * hit popElement (and return to using the parent elements cursor)
   // * hit closeBlock and the next sibling is a close block comment
   //   matching the expected openBlockDepth
+  if (REDEBUG) {
+    /* eslint-disable-next-line no-console */
+    console.warn('disableRehydration', nextSibling);
+  }
   currentCursor.candidate = null;
   currentCursor.nextSibling = nextSibling;
 };
 
-RehydrateBuilder.prototype.enableRehydration = function(candidate) {
-  let currentCursor = this.currentCursor;
+RehydrateBuilder.prototype.enableRehydration = function patchedEnableRehydration(
+  candidate
+) {
+  if (REDEBUG) {
+    /* eslint-disable-next-line no-console */
+    console.log('enableRehydration', candidate);
+  }
+  const { currentCursor } = this;
 
   currentCursor.candidate = candidate;
   currentCursor.nextSibling = null;
 };
 
-
-RehydrateBuilder.prototype.pushElement = function(element, nextSibling) {
-  let cursor = new RehydratingCursor(element, nextSibling, this.blockDepth || 0);
+RehydrateBuilder.prototype.pushElement = function patchedPushElement(
+  element,
+  nextSibling
+) {
+  if (REDEBUG) {
+    /* eslint-disable-next-line no-console */
+    console.log('pushElement', element.tagName, this.blockDepth);
+  }
+  const cursor = new RehydratingCursor(
+    element,
+    nextSibling,
+    this.blockDepth || 0
+  );
 
   /**
    * <div>   <---------------  currentCursor.element
@@ -170,77 +301,110 @@ RehydrateBuilder.prototype.pushElement = function(element, nextSibling) {
   this.cursorStack.push(cursor);
 };
 
+const originalPopElement = RehydrateBuilder.prototype.popElement;
+RehydrateBuilder.prototype.popElement = function patchedPopElement() {
+  if (REDEBUG) {
+    /* eslint-disable-next-line no-console */
+    console.log('popElement', this.element.tagName);
+  }
+  return originalPopElement.apply(this, arguments);
+};
+
 // clears until the end of the current container
 // either the current open block or higher
-RehydrateBuilder.prototype.clearMismatch = function(candidate) {
-    let current = candidate;
-    let currentCursor = this.currentCursor;
-    if (currentCursor !== null) {
-      let openBlockDepth = currentCursor.openBlockDepth;
-      if (openBlockDepth >= currentCursor.startingBlockDepth) {
-        while (current) {
-          if (isCloseBlock(current)) {
-            let closeBlockDepth = getBlockDepth(current);
-            if (openBlockDepth >= closeBlockDepth) {
-              // cleared up until the close but we haven't closed the current
-              // block unless we are above
-              currentCursor.openBlockDepth = closeBlockDepth;
-              break;
-            }
+RehydrateBuilder.prototype.clearMismatch = function patchedClearMismatch(
+  candidate
+) {
+  if (REDEBUG) {
+    /* eslint-disable-next-line no-console */
+    console.warn('clearMismatch', candidate);
+  }
+  let current = candidate;
+  const { currentCursor } = this;
+  if (currentCursor !== null) {
+    const { openBlockDepth } = currentCursor;
+    if (openBlockDepth >= currentCursor.startingBlockDepth) {
+      while (current) {
+        if (isCloseBlock(current)) {
+          const closeBlockDepth = getBlockDepth(current);
+          if (openBlockDepth >= closeBlockDepth) {
+            break;
           }
-          current = this.remove(current);
         }
-      } else {
-        while (current !== null) {
-          current = this.remove(current);
-        }
+        current = this.remove(current);
       }
-      // current cursor parentNode should be openCandidate if element
-      // or openCandidate.parentNode if comment
-      this.disableRehydration(current);
+    } else {
+      while (current !== null) {
+        current = this.remove(current);
+      }
     }
-  };
+    // current cursor parentNode should be openCandidate if element
+    // or openCandidate.parentNode if comment
+    this.disableRehydration(current);
+  }
+};
 
-RehydrateBuilder.prototype.__openBlock = function() {
-  let { currentCursor } = this;
-  if (currentCursor === null) return;
+RehydrateBuilder.prototype.__openBlock = function patchedOpenBlock() {
+  if (REDEBUG) {
+    /* eslint-disable-next-line no-console */
+    console.log('openBlock', this.blockDepth);
+  }
 
-  let blockDepth = this.blockDepth;
+  const { currentCursor } = this;
+  if (currentCursor === null) {
+    return;
+  }
+
+  const { blockDepth } = this;
 
   this.blockDepth++;
 
-  let { candidate } = currentCursor;
-  if (candidate === null) return;
+  const { candidate } = currentCursor;
+  if (candidate === null) {
+    return;
+  }
 
-  let { tagName } = currentCursor.element;
-
+  const { tagName } = currentCursor.element;
   if (isOpenBlock(candidate) && getBlockDepth(candidate) === blockDepth) {
     this.candidate = this.remove(candidate);
     currentCursor.openBlockDepth = blockDepth;
-  } else if (tagName !== 'TITLE' && tagName !== 'SCRIPT' && tagName !== 'STYLE') {
+  } else if (
+    tagName !== 'TITLE' &&
+    tagName !== 'SCRIPT' &&
+    tagName !== 'STYLE'
+  ) {
     this.clearMismatch(candidate);
   }
 };
 
-RehydrateBuilder.prototype.__closeBlock = function() {
-  let { currentCursor } = this;
-  if (currentCursor === null) return;
+RehydrateBuilder.prototype.__closeBlock = function patchedCloseBlock() {
+  if (REDEBUG) {
+    /* eslint-disable-next-line no-console */
+    console.log('closeBlock', this.blockDepth - 1);
+  }
+  const { currentCursor } = this;
+  if (currentCursor === null) {
+    return;
+  }
 
   // openBlock is the last rehydrated open block
-  let openBlockDepth = currentCursor.openBlockDepth;
+  const { openBlockDepth } = currentCursor;
 
   // this currently is the expected next open block depth
   this.blockDepth--;
 
-  let { candidate } = currentCursor;
+  const { candidate } = currentCursor;
 
   let isRehydrating = false;
 
   if (candidate !== null) {
     isRehydrating = true;
 
-    if (isCloseBlock(candidate) && getBlockDepth(candidate) === openBlockDepth) {
-      let nextSibling = this.remove(candidate);
+    if (
+      isCloseBlock(candidate) &&
+      getBlockDepth(candidate) === openBlockDepth
+    ) {
+      const nextSibling = this.remove(candidate);
       this.candidate = nextSibling;
       currentCursor.openBlockDepth--;
     } else {
@@ -256,23 +420,23 @@ RehydrateBuilder.prototype.__closeBlock = function() {
     // check if nextSibling matches our expected close block
     // if so, we remove the close block comment and
     // restore rehydration after clearMismatch disabled
-    let nextSibling = currentCursor.nextSibling;
+    const { nextSibling } = currentCursor;
     if (
       nextSibling !== null &&
       isCloseBlock(nextSibling) &&
-      getBlockDepth(nextSibling) === openBlockDepth
+      getBlockDepth(nextSibling) === this.blockDepth
     ) {
       // restore rehydration state
-      let candidate = this.remove(nextSibling);
-      this.enableRehydration(candidate);
+      const newCandidate = this.remove(nextSibling);
+      this.enableRehydration(newCandidate);
 
       currentCursor.openBlockDepth--;
     }
   }
 };
 
-RehydrateBuilder.prototype.__appendText = function(string) {
-  let { candidate } = this;
+RehydrateBuilder.prototype.__appendText = function patchedAppendText(string) {
+  const { candidate } = this;
 
   if (candidate) {
     if (isTextNode(candidate)) {
@@ -282,26 +446,32 @@ RehydrateBuilder.prototype.__appendText = function(string) {
       this.candidate = candidate.nextSibling;
 
       return candidate;
-    } else if (isSeparator(candidate)) {
-      this.candidate = this.remove(candidate);
-
-      return this.__appendText(string);
-    } else if (isEmpty(candidate) && string === '') {
-      this.candidate = this.remove(candidate);
-
-      return this.__appendText(string);
-    } else {
-      this.clearMismatch(candidate);
-
-      return NewElementBuilder.prototype.__appendText.call(this, string);
     }
-  } else {
-      return NewElementBuilder.prototype.__appendText.call(this, string);
-  }
-}
+    if (isSeparator(candidate)) {
+      this.candidate = this.remove(candidate);
 
-RehydrateBuilder.prototype.__pushRemoteElement = function(element, cursorId, insertBefore) {
-  let marker = this.getMarker(element, cursorId);
+      return this.__appendText(string);
+    }
+    if (isEmpty(candidate) && string === '') {
+      this.candidate = this.remove(candidate);
+
+      return this.__appendText(string);
+    }
+    this.clearMismatch(candidate);
+
+    return NewElementBuilder.prototype.__appendText.call(this, string);
+  }
+
+  return NewElementBuilder.prototype.__appendText.call(this, string);
+};
+
+RehydrateBuilder.prototype.__pushRemoteElement = function patchedPushRemoteElement(
+  element,
+  cursorId,
+  _insertBefore
+) {
+  const marker = this.getMarker(element, cursorId);
+  let insertBefore = _insertBefore;
 
   // when insertBefore is not present, we clear the element
   if (insertBefore === undefined) {
@@ -311,7 +481,7 @@ RehydrateBuilder.prototype.__pushRemoteElement = function(element, cursorId, ins
     insertBefore = null;
   }
 
-  let cursor = new RehydratingCursor(element, null, this.blockDepth);
+  const cursor = new RehydratingCursor(element, null, this.blockDepth);
   this.cursorStack.push(cursor);
 
   if (marker === null) {
@@ -320,57 +490,12 @@ RehydrateBuilder.prototype.__pushRemoteElement = function(element, cursorId, ins
     this.candidate = this.remove(marker);
   }
 
-  let tracker = new RemoteBlockTracker(element);
+  const tracker = new RemoteBlockTracker(element);
   this.pushBlockTracker(tracker, true);
 };
 
-function isOpenBlock(node) {
-  return node.nodeType === 8 && node.nodeValue.lastIndexOf('%+b:', 0) === 0;
-}
-
-function isCloseBlock(node) {
-  return node.nodeType === 8 && node.nodeValue.lastIndexOf('%-b:', 0) === 0;
-}
-
-function getBlockDepth(node) {
-  return parseInt(node.nodeValue.slice(4), 10);
-}
-
 /* end code changes from https://github.com/glimmerjs/glimmer-vm/pull/988 */
 
-// from https://github.com/glimmerjs/glimmer-vm/blob/v0.35.11/packages/@glimmer/runtime/lib/vm/rehydrate-builder.ts#L440
-function isSeparator(node) {
-  return node.nodeType === 8 && node.nodeValue === '%|%';
-}
-
-// from https://github.com/glimmerjs/glimmer-vm/blob/v0.35.11/packages/@glimmer/runtime/lib/vm/rehydrate-builder.ts#L444
-function isEmpty(node) {
-  return node.nodeType === 8 && node.nodeValue === '% %';
-}
-
-// from https://github.com/glimmerjs/glimmer-vm/blob/v0.35.11/packages/@glimmer/runtime/lib/vm/rehydrate-builder.ts#L404
-function isTextNode(node) {
-  return node.nodeType === 3;
-}
-
-// from https://github.com/glimmerjs/glimmer-vm/blob/v0.35.11/packages/%40glimmer/runtime/lib/bounds.ts#L80
-function clear(bounds) {
-  let parent = bounds.parentElement();
-  let first = bounds.firstNode();
-  let last = bounds.lastNode();
-
-  let node = first;
-
-  while (node) {
-    let next = node.nextSibling;
-    parent.removeChild(node);
-    if (node === last) return next;
-    node = next;
-  }
-
-  return null;
-}
-
 export default {
-  initialize() {}
+  initialize() {},
 };
